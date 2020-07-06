f41d1b463882067ae730cb9951302ad3fa053f6a
mayurnovitekk <mayur.novitekk@gmail.com>
2020-07-06 21:41:14 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/images'));
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs');
+});
+
+app.listen(3000, () => {
+  console.log('Start port 3000');
+});
+
